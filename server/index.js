1b147c62b4c3717cda2d9dcfd3c5abe450b98c3e
CasstielP <cpss5433@gmail.com>
2025-03-27 13:04:04 -0700

    add authRoutes in backend

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const seedDevices = require("./seeders/seedDevices")
 const seedConfig = require('./seeders/seedConfig')
 const configRoutes = require("./routes/configRoutes")
 const seedUsers = require('./seeders/seedUsers')
+const authRoutes = require('./routes/authRoutes')
 dotenv.config()
 
 const app = express()
@@ -14,6 +15,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/devices', deviceRoutes)
 app.use('/api/config', configRoutes)
+app.use('/api', authRoutes)
 
 
 const PORT = process.env.PORT || 8000;
