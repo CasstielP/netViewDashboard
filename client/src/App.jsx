109a93c82559b53eb6dea3f06ea61a79d0ea3c7a
CasstielP <cpss5433@gmail.com>
2025-03-27 13:35:22 -0700

    add login session presist logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/DashBoard";
 import ConfigPage from "./pages/configPage";
+import Login from "./pages/Login";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchMe } from "./features/auth/authSlice";
+
+
 
 
 const App = () => {
+  const dispatch = useDispatch()
+  const {user} = useSelector((state)=> state.auth)
+
+  useEffect(()=> {
+    const token = localStorage.getItem('token')
+    if(token) {
+      dispatch(fetchMe())
+    }
+  }, [dispatch])
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/config" element={<ConfigPage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/config" element={user ? <ConfigPage /> : <Login />} />
       </Routes>
       
